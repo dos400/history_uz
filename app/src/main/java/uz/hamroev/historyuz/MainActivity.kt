5f6770a84cb5b2f7eb807f67599373d36939766f
doston <dos400dos400@gmail.com>
2022-11-19 00:01:47 +0500

    first commit

--- app/src/main/java/uz/hamroev/historyuz/MainActivity.kt
+++ app/src/main/java/uz/hamroev/historyuz/MainActivity.kt
@@ -1,14 +1,34 @@
 package uz.hamroev.historyuz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
+import uz.hamroev.historyuz.activity.HomeActivity
+import uz.hamroev.historyuz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         /* Doston Hamroyev*/
+        /*Bismillahir rohmanir rohim Alloh o'zing madadkor bo'l */
+
+        Handler(Looper.getMainLooper()).postDelayed(object : Runnable {
+            override fun run() {
+                startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+            }
+        }, 1500)
+
+
+
+
     }
 }
 
---
+++ app/src/main/java/uz/hamroev/historyuz/activity/HomeActivity.kt
@@ -0,0 +1,16 @@
+package uz.hamroev.historyuz.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import uz.hamroev.historyuz.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
